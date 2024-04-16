81e0d804775193b6f820adb798cbbe248b5119fb
my-git-username <118355685+my-git-username@users.noreply.github.com>
2024-04-16 18:42:01 +0300

    Solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,41 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    operate(action, num) {
+      action.call(this, num);
+
+      return calculator;
+    },
+    add(num) {
+      this.result += num;
+
+      return calculator;
+    },
+    subtract(num) {
+      this.result -= num;
+
+      return calculator;
+    },
+    multiply(num) {
+      this.result *= num;
+
+      return calculator;
+    },
+    divide(num) {
+      this.result /= num;
+
+      return calculator;
+    },
+    reset() {
+      this.result = 0;
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
